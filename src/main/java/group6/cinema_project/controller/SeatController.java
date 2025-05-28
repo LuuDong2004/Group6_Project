3c7a37984c92b413135419a3ed0c99a5e7a71d4e
luudong <luuvandong2004@gmail.com>
2025-05-28 14:04:30 +0700

    fix

--- src/main/java/group6/cinema_project/controller/SeatController.java
+++ src/main/java/group6/cinema_project/controller/SeatController.java
@@ -1,4 +1,27 @@
 package group6.cinema_project.controller;
 
+import group6.cinema_project.dto.SeatDto;
+import group6.cinema_project.dto.TicketDto;
+import group6.cinema_project.service.ISeatService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import java.util.List;
+
+@Controller
+@RequestMapping("api/seats")
 public class SeatController {
+    @Autowired
+    private ISeatService seatService;
+
+    @GetMapping("get/{sId}")
+    public ResponseEntity<List<SeatDto>> getSeatsByScheduleId(@PathVariable(name = "sId") Integer scheduleId) {
+        List<SeatDto> seats = seatService.getSeatsByScheduleId(scheduleId);
+        return ResponseEntity.ok(seats);
+    }
 }
